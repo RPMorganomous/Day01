66b405bd136b4cf070913c314b8aa70d2ec3745b
RPMorganomous <mba.rick@gmail.com>
2018-12-08 17:35:54 -0600

    adds numbers from file

--- Program.cs
+++ Program.cs
@@ -7,19 +7,23 @@ namespace ConsoleApplication1
 	{
 		public static void Main(string[] args)
 		{
-			Console.WriteLine("howdy, y'all");
+			Console.WriteLine("devan should play with me");
 			
 			StreamReader myReader = new StreamReader("D:\\developement\\Advent2018\\Day01\\files\\day01input.txt");
 			string line = "";
+			int num = 0;
 
 			while (line != null)
 			{
 				line = myReader.ReadLine();
 				if (line!= null)
 					Console.WriteLine(line);
+					num = num + Convert.ToInt32(line);
+					Console.WriteLine(num);
+
 			}
 			myReader.Close();
-			Console.WriteLine("done");
+			Console.WriteLine("done = " + num);
 			Console.ReadLine();
 			
 		}
