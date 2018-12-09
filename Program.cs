1bde974bc336079a6600cb9c94e9f494b0a00bda
RPMorganomous <mba.rick@gmail.com>
2018-12-08 18:47:13 -0600

    creating a list

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Runtime.InteropServices;
 
 namespace ConsoleApplication1
 {
@@ -12,19 +14,37 @@ namespace ConsoleApplication1
 			StreamReader myReader = new StreamReader("D:\\developement\\Advent2018\\Day01\\files\\day01input.txt");
 			string line = "";
 			int num = 0;
+			List<int> theList = new List<int>();
+			var alreadyInTheList = false;
 
-			while (line != null)
+			while (!alreadyInTheList)
 			{
-				line = myReader.ReadLine();
-				if (line!= null)
-					Console.WriteLine(line);
-					num = num + Convert.ToInt32(line);
-					Console.WriteLine(num);
-
+				while (line != null)
+				{
+					line = myReader.ReadLine();
+					if (line != null)
+						{
+						Console.WriteLine(line);
+						num = num + Convert.ToInt32(line);
+						Console.WriteLine(num);
+						alreadyInTheList = theList.Contains(num);
+						if (alreadyInTheList)
+							{
+								Console.WriteLine("alreadyInTheList done = " + num + line);
+								break;
+							}
+						theList.Add(num);
+						}
+					//line = "";	
+					//alreadyInTheList = false;				
+				}
+				myReader = new StreamReader("D:\\developement\\Advent2018\\Day01\\files\\day01input.txt");
+				line = "";
 			}
+
 			myReader.Close();
-			Console.WriteLine("done = " + num);
-			Console.ReadLine();
+			Console.WriteLine("done = " + num + line);
+			//Console.ReadLine();
 			
 		}
 	}
